f3dbd52ee3b97ba97b074379c696c9c044acea6c
ArturoAlcaniz <arturoguijarro@hotmail.com>
2022-11-24 23:04:16 +0000

    Fix build users

--- JenkinsService/install.sh
+++ JenkinsService/install.sh
@@ -3,6 +3,7 @@
 # Commands are only run once even if the script is called multiple times.
 curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb
 dpkg -i cloudflared.deb
+systemctl start docker
 cloudflared service install eyJhIjoiOGI3ZjY1YTgxY2E3ZmM1NGFhZDZjNWU2NWQyMzUwNzMiLCJ0IjoiNmI2NjJiNjQtMzcyMi00OTA4LWEwYzQtM2Y0ODJjMzAwMjUxIiwicyI6Ik9HSTBZbUpoT0RBdE5tSTROeTAwWkdRMkxXSTFNelV0TldSbVptUTRaV1ExWW1GayJ9
 service jenkins start
 tail -f /var/log/jenkins/jenkins.log
