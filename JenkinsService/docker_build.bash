ba6e8d2beff4bdaa27d2a20acc349a159a389d85
ArturoAlcaniz <arturoguijarro@hotmail.com>
2022-11-25 19:04:17 +0000

    Fixed build UsersService

--- JenkinsService/docker_build.bash
+++ JenkinsService/docker_build.bash
@@ -1,2 +1,3 @@
 docker build -t arturoalcaniz/jenkins-service -f Dockerfile .. --network host
+printf $1 | docker login --username arturoalcaniz --password-stdin
 docker push arturoalcaniz/jenkins-service
