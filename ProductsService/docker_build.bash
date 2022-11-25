ec3f29829cc83718b0c344d82e2fe93e2e327102
ArturoAlcaniz <arturoguijarro@hotmail.com>
2022-11-25 20:10:59 +0000

    Added all builds

--- ProductsService/docker_build.bash
+++ ProductsService/docker_build.bash
@@ -1,2 +1,6 @@
 docker build -t arturoalcaniz/products-service -f Dockerfile ..
+if [ "$1" ]
+  then
+    printf $1 | docker login --username arturoalcaniz --password-stdin
+fi
 docker push arturoalcaniz/products-service
