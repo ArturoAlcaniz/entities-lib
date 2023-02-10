ad9c56cefe3984194112dcfb223ccd39786f61a5
ArturoAlcaniz <arturoguijarro@hotmail.com>
2023-02-10 21:33:34 +0000

    Added Fix versionctsService versi

--- ProductsService/docker_build.bash
+++ ProductsService/docker_build.bash
@@ -1,4 +1,4 @@
-docker build -t arturoalcaniz/products-service:$(npm pkg get version) -f Dockerfile ..
+docker build -t arturoalcaniz/products-service:$(npm pkg get version | tr -d '"') -f Dockerfile ..
 if [ "$1" ]
   then
     printf $1 | docker login --username arturoalcaniz --password-stdin
