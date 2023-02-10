4851d292098c8b875f99797eda493b6b0d9951c9
ArturoAlcaniz <arturoguijarro@hotmail.com>
2023-02-10 22:21:18 +0000

    Added latest versioned

--- ProductsService/docker_build.bash
+++ ProductsService/docker_build.bash
@@ -1,6 +1,7 @@
-docker build -t arturoalcaniz/products-service:$(npm pkg get version | tr -d '"') -f Dockerfile ..
+docker build -t arturoalcaniz/products-service:latest -t arturoalcaniz/products-service:$(npm pkg get version | tr -d '"') -f Dockerfile ..
 if [ "$1" ]
   then
     printf $1 | docker login --username arturoalcaniz --password-stdin
 fi
 docker push arturoalcaniz/products-service:$(npm pkg get version | tr -d '"')
+docker push arturoalcaniz/products-service:latest
--- UsersService/docker_build.bash
+++ UsersService/docker_build.bash
@@ -1,6 +1,7 @@
-docker build -t arturoalcaniz/users-service:$(npm pkg get version | tr -d '"') -f Dockerfile ..
+docker build -t arturoalcaniz/users-service:latest -t arturoalcaniz/users-service:$(npm pkg get version | tr -d '"') -f Dockerfile ..
 if [ "$1" ]
   then
     printf $1 | docker login --username arturoalcaniz --password-stdin
 fi
 docker push arturoalcaniz/users-service:$(npm pkg get version | tr -d '"')
+docker push arturoalcaniz/users-service:latest
