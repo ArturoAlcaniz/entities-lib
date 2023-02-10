34c96b3eb63f443eef58b17d328639f411f0642e
ArturoAlcaniz <arturoguijarro@hotmail.com>
2023-02-10 21:29:06 +0000

    Added ProductsService versioned

--- ProductsService/docker_build.bash
+++ ProductsService/docker_build.bash
@@ -1,4 +1,4 @@
-docker build -t arturoalcaniz/products-service -f Dockerfile ..
+docker build -t arturoalcaniz/products-service:$(npm pkg get version) -f Dockerfile ..
 if [ "$1" ]
   then
     printf $1 | docker login --username arturoalcaniz --password-stdin
